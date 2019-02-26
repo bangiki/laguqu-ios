c2f97e3143951eeac19a83358d2eeac1a570cd86
bangiki <ramdani.rizki19@gmail.com>
2019-02-26 09:15:31 +0700

    add extention UIAlertViewController

--- laguqu-ios/Domainless/Extention/UIAlertViewController.swift
+++ laguqu-ios/Domainless/Extention/UIAlertViewController.swift
@@ -7,3 +7,25 @@
 //
 
 import Foundation
+
+extension UIAlertController {
+  static func show(title: String? = nil, message: String, completion: (() -> Void)? = nil) {
+    let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+    
+    alert.addAction(UIAlertAction(title: "OK", style: .destructive, handler: { (_) in
+      completion?()
+    }))
+    
+    alert.present(animated: true, completion: nil)
+  }
+  
+  func present(animated: Bool, completion: (() -> Void)?) {
+    if let rootVC = UIApplication.shared.keyWindow?.rootViewController {
+      if rootVC.presentedViewController == nil {
+        rootVC.present(self, animated: true, completion: completion)
+      } else {
+        rootVC.presentedViewController?.present(self, animated: true, completion: nil)
+      }
+    }
+  }
+}
