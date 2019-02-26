4f0d67b15f128c25afb0c573ebd0b134db318634
bangiki <ramdani.rizki19@gmail.com>
2019-02-26 09:23:33 +0700

    implement listener add to wishlist

--- laguqu-ios/Feature/FindLyrics/Cell/ArtisAlbumCell.swift
+++ laguqu-ios/Feature/FindLyrics/Cell/ArtisAlbumCell.swift
@@ -12,27 +12,37 @@ class ArtisAlbumCell: UITableViewCell {
   
   @IBOutlet weak var nameArtist: UILabel!
   @IBOutlet weak var rating: UILabel!
+  @IBOutlet weak var btnAddWishlist: UIButton!
   
   static let identifier = "ArtisAlbumCell"
   static let height : CGFloat = 85
+
+  var listener     : ((Artist) -> Void)?
   
   var item: Artist? {
     didSet {
       updateContent()
     }
   }
-  
+
   override func awakeFromNib() {
     super.awakeFromNib()
     // Initialization code
+    btnAddWishlist.setRadius(radius: 3)
   }
   
   private func updateContent(){
+    
     self.nameArtist.text = item?.artistName
     guard let rating = item?.artistRating else {
       return
     }
     self.rating.text = "Rating: \(rating)"
+    
   }
 
+  @IBAction func addToWishList(_ sender: UIButton) {
+    listener?(item!)
+  }
+  
 }
